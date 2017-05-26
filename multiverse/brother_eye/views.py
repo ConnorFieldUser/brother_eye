ccadba405f2e02255ae4fbe15d9f18742aa28116
ConnorFieldUser <conquest.field@gmail.com>
2017-05-26 16:08:52 -0400

    landing page, files, auth

--- multiverse/brother_eye/views.py
+++ multiverse/brother_eye/views.py
@@ -5,5 +5,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
+
 class LandingView(TemplateView):
     template_name = 'index.html'
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
